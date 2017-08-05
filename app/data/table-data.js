4b1d907e0f034928b7a4976a12dd8d24f1f14517
carlosalfonzo33 <carlos.alfonzo@gmail.com>
2017-08-05 02:30:10 -0700

    added table-data and waitinglist js files

---
+++ app/data/table-data.js
@@ -0,0 +1,10 @@
+var tableArray = [
+	{
+		customerName: 'Carlos',
+		customerEmail: 'carlos@example.com',
+		customerID: 'carlos1',
+		phoneNumber: '000-000-0000'
+	}
+];
+
+module.exports = tableArray;
---
+++ app/data/waitinglist-data.js
@@ -0,0 +1,10 @@
+var waitingArray = [
+	{
+		customerName: 'Charlie',
+		customerEmail: 'charlie@example.com',
+		customerID: 'charlie2',
+		phoneNumber: '000-000-0000'
+	}
+];
+
+module.exports = waitingArray;
