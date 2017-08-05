c87810cef8aab08e1c8d09f1aaf615feaa6e9fe3
carlosalfonzo33 <carlos.alfonzo@gmail.com>
2017-08-05 02:51:29 -0700

    fixed server.js file

---
+++ app/routing/html-routes.js
@@ -0,0 +1,17 @@
+var path = require('path');
+
+module.exports = function (app) {
+
+	// viewed at http://localhost:8080
+app.get('/tables', function(req, res) {
+    res.sendFile(path.join(__dirname + '/../public/tables.html'));
+	});
+
+app.get('/reserve', function(req, res) {
+    res.sendFile(path.join(__dirname + '/../public/reserve.html'));
+	});
+
+app.use(  function(req, res) {
+    res.sendFile(path.join(__dirname + '/../public/home.html'));
+	});
+}
