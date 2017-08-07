fa58bbf1f2ed255dfa7471985193be3519f010d7
carlosalfonzo33 <carlos.alfonzo@gmail.com>
2017-08-06 22:42:58 -0700

    added data to table and waiting lists

--- app/data/table-data.js
+++ app/data/table-data.js
@@ -1,3 +1,12 @@
-var tableArray = [];
+var tableArray = [
+{
+		customerName: "Carlos",
+		customerEmail: "carlos@test.com",
+		customerID: "carlos1",
+		phoneNumber: "111-111-1111",
+
+	}
+
+];
 
 module.exports = tableArray;
--- app/data/waitinglist-data.js
+++ app/data/waitinglist-data.js
@@ -1,3 +1,11 @@
-var waitingArray = [];
+var waitingArray = [
+	{
+		customerName: "Carlos",
+		customerEmail: "carlos@test.com",
+		phoneNumber: "111-111-1111",
+		customerID: "carlos"
+	}
+
+];
 
 module.exports = waitingArray;
